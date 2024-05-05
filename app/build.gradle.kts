b6eae5bf388ca58d4dc0d7d27bd14f9305cc210c
Diogo Reis <diogo.reis@ipvc.pt>
2024-05-06 00:00:46 +0100

    Tabs Funcional

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,11 +78,13 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
+
     implementation("androidx.room:room-common:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation ("androidx.room:room-runtime:2.6.1")
     kapt ("androidx.room:room-compiler:2.6.1")
     testImplementation("androidx.room:room-testing:2.6.1")
+    implementation ("com.google.android.material:material:1.5.0")
 
 
 }
