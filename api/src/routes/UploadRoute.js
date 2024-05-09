517e1e02c9b49d6775ada3becd1dc2b8d5beb147
Ricardo Gonçalves <rcerqueiragoncalves@ipvc.pt>
2024-05-09 17:11:24 +0100

    Tentativa 6 de upload

--- api/src/routes/UploadRoute.js
+++ api/src/routes/UploadRoute.js
@@ -1,38 +1,22 @@
 const express = require('express');
 const { put } = require('@vercel/blob');
 const fs = require('fs');
-const path = require('path');
+const path = require('path'); // Importe o módulo path
 
 const router = express.Router();
 
 router.post('/upload', async (req, res) => {
   try {
-    // Verifica se o corpo da requisição contém o parâmetro 'filePath'
-    const filePathFromBody = req.body.filePath;
 
-    if (!filePathFromBody) {
-      // Se o parâmetro 'filePath' não estiver presente no corpo da requisição, retorna um erro
-      return res.status(400).json({ error: 'O parâmetro "filePath" é obrigatório' });
-    }
+    const filePath = path.join(__dirname, '../../images/imagegame.png'); // Use o módulo path para construir o caminho
 
-    // Constrói o caminho completo da imagem usando o caminho fornecido no corpo da requisição
-    const filePath = path.join(__dirname, '..', '..', filePathFromBody);
 
-    // Verifica se o arquivo especificado existe
-    if (!fs.existsSync(filePath)) {
-      return res.status(404).json({ error: 'Arquivo não encontrado' });
-    }
-
-    // Lê o conteúdo do arquivo
     const fileContent = fs.readFileSync(filePath);
 
-    // Extrai o nome do arquivo da parte final do caminho
-    const fileName = path.basename(filePath);
 
-    // Armazena o arquivo na Vercel
-    const blob = await put(fileName, fileContent, { access: 'public' });
+    const blob = await put('image.png', fileContent, { access: 'public' });
 
-    // Retorna a URL do arquivo armazenado
+
     res.json({ url: blob.url });
   } catch (error) {
     console.error(error);
