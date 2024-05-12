4bc229733dab8590e80de2b95d711e46414aa99a
Ricardo Gonçalves <rcerqueiragoncalves@ipvc.pt>
2024-05-12 14:23:04 +0100

    mais uma tentativa 4

--- api/src/routes/UploadRoute.js
+++ api/src/routes/UploadRoute.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const { put } = require('@vercel/blob');
-const fs = require('fs');
+const multer = require('multer');
 const path = require('path');
 
 const router = express.Router();
 
-router.post('/upload', async (req, res) => {
-  try {
-    const imageContent = req.body.imageContent;
-    const imageName = req.body.imageName;
+// Configurando o armazenamento com multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-    /*
-    if (!imageContent || !imageName) {
-      return res.status(400).json({ error: 'O conteúdo e o nome da imagem são obrigatórios no corpo da requisição' });
+router.post('/upload', upload.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'Nenhuma imagem enviada' });
     }
-    */
+
+    const imageContent = req.file.buffer;
+    const imageName = req.file.originalname;
 
     const blob = await put(imageName, imageContent, { access: 'public' });
 
     res.json({ url: blob.url });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ error: 'Ocorreu um erro no servidor' });
   }
 });
 
