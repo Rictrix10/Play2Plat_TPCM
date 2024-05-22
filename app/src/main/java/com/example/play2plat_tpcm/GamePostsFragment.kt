3c8422830738083148bb2a2f454b3886d18d027d
Ricardo Gonçalves <rcerqueiragoncalves@ipvc.pt>
2024-05-22 16:17:41 +0100

    listagem de posts

--- app/src/main/java/com/example/play2plat_tpcm/GamePostsFragment.kt
+++ app/src/main/java/com/example/play2plat_tpcm/GamePostsFragment.kt
@@ -1,15 +1,22 @@
 package com.example.play2plat_tpcm
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.play2plat_tpcm.api.GamePost
+import com.example.play2plat_tpcm.api.ApiManager
+import com.example.play2plat_tpcm.api.GameCommentsResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class GamePostsFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,26 +26,42 @@ class GamePostsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // Inflate o layout para este fragmento
         val view = inflater.inflate(R.layout.fragment_game_posts, container, false)
 
-        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView = view.findViewById(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = GamePostsAdapter(getGamePosts())
+
+        // Chama a API para obter os posts do jogo
+        getGamePosts()
 
         return view
     }
 
-    private fun getGamePosts(): List<GamePost> {
-        // Esta função deve retornar a lista de posts. Aqui estamos adicionando exemplos fictícios.
-        return listOf(
-            GamePost(R.drawable.noimageuser, "User1", "This is a post text", R.drawable.email),
-            GamePost(R.drawable.noimageuser, "User2", "Another post text", R.drawable.facebook),
-            GamePost(R.drawable.noimageuser, "User3", "This is a post text 2", R.drawable.email),
-            GamePost(R.drawable.noimageuser, "User4", "Another post text 2", R.drawable.facebook),
-            GamePost(R.drawable.noimageuser, "User5", "This is a post text 3", R.drawable.email),
-            GamePost(R.drawable.noimageuser, "User6", "Another post text 3", R.drawable.facebook)
-        )
+    private fun getGamePosts() {
+        val gameId = 1 // Defina o ID do jogo para obter os comentários
+        ApiManager.apiService.getCommentByGame(gameId).enqueue(object : Callback<List<GameCommentsResponse>> {
+            override fun onResponse(
+                call: Call<List<GameCommentsResponse>>,
+                response: Response<List<GameCommentsResponse>>
+            ) {
+                if (response.isSuccessful) {
+                    val gamePosts = response.body()
+                    Log.d("GamePostsFragment", "Resposta da API: $gamePosts")
+                    if (gamePosts != null) {
+                        recyclerView.adapter = GamePostsAdapter(gamePosts)
+                    }
+                } else {
+                    Log.e("GamePostsFragment", "Erro na resposta: ${response.errorBody()}")
+                    // Tratar erro
+                }
+            }
+
+            override fun onFailure(call: Call<List<GameCommentsResponse>>, t: Throwable) {
+                Log.e("GamePostsFragment", "Falha na chamada da API: ${t.message}")
+                // Tratar falha
+            }
+        })
     }
 
     companion object {
---
+++ app/src/main/java/com/example/play2plat_tpcm/api/GameComment.kt
@@ -0,0 +1,5 @@
+package com.example.play2plat_tpcm.api
+
+data class GameComment(
+    val name: String
+)
