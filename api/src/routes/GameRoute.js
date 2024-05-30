43c68c18e6d1798b1524b3f982f29c71eee91de7
Ricardo Gonçalves <rcerqueiragoncalves@ipvc.pt>
2024-05-30 10:46:41 +0100

    tentativa 1

--- api/src/routes/GameRoute.js
+++ api/src/routes/GameRoute.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const GameController = require('../controllers/GameController');
 
+router.get('/games/filtered-games', GameController.getFilteredGames);
+
 router.post('/games', GameController.createGame);
 router.get('/games', GameController.getGames);
 router.get('/games/:id', GameController.getGameById);
 router.patch('/games/:id', GameController.updateGame);
 router.delete('/games/:id', GameController.deleteGame);
-router.get('/games/filtered-games', GameController.getFilteredGames);
+
 module.exports = router;
