f3e29ab82039d04b5ec3457138ea700a4d803a97
Ricardo Gonçalves <rcerqueiragoncalves@ipvc.pt>
2024-06-10 17:48:31 +0100

    delete de imagens via vercel blob 2

--- api/src/routes/UploadRoute.js
+++ api/src/routes/UploadRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const { put } = require('@vercel/blob');
+const { put, del } = require('@vercel/blob');
 
 const router = express.Router();
 const upload = multer();
