972c4bd7c1c0c1630a3f6e624d88cca6bd9a4a91
Ricardo Gonçalves <rcerqueiragoncalves@ipvc.pt>
2024-06-21 20:48:06 +0100

    endpoints random v3

--- api/src/models/GenreModel.js
+++ api/src/models/GenreModel.js
@@ -73,7 +73,7 @@ const GenreModel = {
             });
 
             // Filtre os gêneros que têm pelo menos 7 jogos associados
-            const validGenres = genresWithCounts.filter(genre => genre._count.games >= 7);
+            const validGenres = genresWithCounts.filter(genre => genre._count.games >= 3);
 
             if (validGenres.length === 0) {
                 return [];
